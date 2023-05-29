0255c249ea01e59c86e2b7ec126a70900afccc29
Caio Fortes dos Santos <caiofortesprofissional@gmail.com>
2023-05-28 23:53:54 -0300

    check

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const express = require('express');
 
 const app = express();
 // Crie um endpoint para receber requisições
-app.get('/', function(req, res) {
+app.get('/endpoint', function(req, res) {
   res.send('Ola Mundo!');
 });
 
 // Imprima URL para acessar o servidor
 app.listen(8000, function() {
   console.log('App ouvindo na porta 8000!');
+});
+
+app.post('/post', (req, res) => {
+  res.send(`POST concluido`);
+});
+
+app.delete('/deletar', (req, res) => {
+  res.send(`delete concluido`);
 });
